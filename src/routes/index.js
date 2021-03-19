ea026761d846373b65b14b0c4c0c5260c30aef89
liuwenzhu <547485261@qq.com>
2021-03-19 11:04:32 +0800

    save

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,20 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
-    throw new Error("参数错误")
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
+    throw new Error('参数错误')
+    await ctx.render('index', {
+        title: 'Hello Koa 2!'
+    })
 })
 
 router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
+    ctx.body = 'koa2 string'
 })
 
 router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
+    ctx.body = {
+        title: 'koa2 json'
+    }
 })
 
 module.exports = router
