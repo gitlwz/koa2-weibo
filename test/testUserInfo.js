78ef434d38153b7469584e9c1882069a8417518d
liuwenzhu <547485261@qq.com>
2021-03-30 14:43:34 +0800

    test:单元测试创建

---
+++ test/testUserInfo.js
@@ -0,0 +1,15 @@
+
+/**
+ * 【特别提醒】cookie 是用户的敏感信息，此处只能是**测试**用户的 cookie
+ * 每次测试用户重新登录，都需要更新这里的 cookie
+ */
+
+module.exports = {
+    Z_ID: 3,
+    Z_USER_NAME: 'lisi',
+    Z_COOKIE: 'weibo.sid=PTmO5LP_hQm1HCbFnzrvTLL6xrWnX7Km; weibo.sid.sig=Huo7S9yNyYO6ppDs3IAMkqujTV8; token=68c9fdc9-b7e4-4a5e-a451-d6c07a153b1c',
+
+    L_ID: 2,
+    L_USER_NAME: 'lisi',
+    L_COOKIE: 'weibo.sid=686m0u92zMTCmmSQY85trYv_PScDgpFx; weibo.sid.sig=Za_Oj-wSLFrG5QzQhqQU0YQxOFo'
+}
