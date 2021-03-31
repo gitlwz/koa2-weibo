00b9a9024d81f951a1ba51d603cbe5ebb1d3c8f5
liuwenzhu <547485261@qq.com>
2021-03-31 10:18:30 +0800

    test:首页单元测试

--- test/blog/home.test.js
+++ test/blog/home.test.js
@@ -7,7 +7,7 @@ let BLOG_ID = ''
 test('创建一条微博，应该成功', async () => {
     // 定义测试内容
     const content = '单元测试自动创建的微博_' + Date.now()
-    const image = 'https://picsum.photos/300/300'
+    const image = '/xxx.png'
 
     // 开始测试
     const res = await server
@@ -23,4 +23,18 @@ test('创建一条微博，应该成功', async () => {
 
     // 记录微博 id
     BLOG_ID = res.body.data.id
+})
+
+// 加载第一页数据
+test('首页，加载第一页数据', async () => {
+    const res = await server
+                    .get(`/api/blog/loadMore/0`)
+                    .set('cookie', Z_COOKIE)  // 设置 cookie
+    expect(res.body.errno).toBe(0)
+    const data = res.body.data
+    expect(data).toHaveProperty('isEmpty')
+    expect(data).toHaveProperty('blogList')
+    expect(data).toHaveProperty('pageSize')
+    expect(data).toHaveProperty('pageIndex')
+    expect(data).toHaveProperty('count')
 })
