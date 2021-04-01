33445a47ae3fa86ef22c28781cb0337708329b40
liuwenzhu <547485261@qq.com>
2021-04-01 15:41:34 +0800

    add：@功能

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -4,7 +4,7 @@ const userValidate = require('../../validator/user')
 const { genValidator } = require("../../middlewares/validator")
 const { isTest } = require('../../utils/env')
 const { loginCheck } = require('../../middlewares/loginChecks')
-
+const { getFollowers } = require('../../controller/user-relation')
 router.prefix("/api/user")
 
 //注册路由
@@ -57,5 +57,17 @@ router.patch('/changePassword', loginCheck, genValidator(userValidate), async (c
 // 退出登录
 router.post('/logout', loginCheck, async (ctx, next) => {
     ctx.body = await logout(ctx)
+})
+
+// 获取 at 列表，即关注人列表
+router.get('/getAtList', loginCheck, async (ctx, next) => {
+    const { id: userId } = ctx.session.userInfo
+    const result = await getFollowers(userId)
+    const { followersList } = result.data
+    const list = followersList.map(user => {
+        return `${user.nickName} - ${user.userName}`
+    })
+    // 格式如 ['张三 - zhangsan', '李四 - lisi', '昵称 - userName']
+    ctx.body = list
 })
 module.exports = router
